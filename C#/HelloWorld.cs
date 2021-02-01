e9ec5b67a583f6e2dbed29cf712e416628ac567e
lennykioko <lennykmutua@gmail.com>
2021-02-01 14:19:06 +0300

    Complete C sharp

---
+++ C#/HelloWorld.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var anything = "Hello World";
+            Console.WriteLine(anything);
+        }
+    }
+}
